d0ad0fa3de8e7512043c4b0fc1ea302dcc4c5ff7
Adroso360 <adrian.lapico@my.jcu.edu.au>
2016-07-31 23:05:49 +1000

    edits

--- DebuggingExercises/FixDebugOne2.java
+++ DebuggingExercises/FixDebugOne2.java
@@ -1,6 +1,6 @@
 public class FixDebugOne2
 {
-   /* This program displays some output*/
+   /* This program displays some output */
    public static void main(String[] args)
    {
       System.out.println("Java programming is fun.");
